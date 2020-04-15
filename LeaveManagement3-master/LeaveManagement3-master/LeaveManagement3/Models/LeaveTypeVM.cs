d236f972ad194af009ea95a3b12ef3b596ff18bb
nitin leuva <mr.leuva@gmail.com>
2020-04-15 04:47:30 -0500

    adding Display attr to view model leave tpe

---
+++ LeaveManagement3-master/LeaveManagement3-master/LeaveManagement3/Models/LeaveTypeVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagement3.Models
+{
+    public class DetailsLeaveTypeVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Display (Name = "Date Created")]
+        public DateTime DateCreated { get; set; }
+    }
+
+    public class CreateLeaveTypeVM
+    {
+        [Required]
+        public string Name { get; set; }
+        
+    }
+}
