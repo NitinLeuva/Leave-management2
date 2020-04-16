a61fc65a8a6d6e6e84fe09030e9ee13fb6693fc5
nitin leuva <mr.leuva@gmail.com>
2020-04-16 02:41:16 -0500

    test commit

--- LeaveManagement3-master/LeaveManagement3-master/LeaveManagement3/Models/EmployeeVM.cs
+++ LeaveManagement3-master/LeaveManagement3-master/LeaveManagement3/Models/EmployeeVM.cs
@@ -8,7 +8,7 @@ namespace LeaveManagement3.Models
     public class EmployeeVM
     {
         public string Id { get; set; }
-        public string UserName { get; set; }
+        public string UserName { get; set;  }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Firstname { get; set; }
