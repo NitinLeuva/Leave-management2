c954b5074b6d74e6d51d4da540287387418bc8e0
NitinLeuva <mr.leuva@gmail.com>
2020-04-20 17:08:36 +0530

    role and usrt

---
+++ LeaveManagement3-master/LeaveManagement3-master/LeaveManagement3/SeedData.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagement3
+{
+    public static class SeedData
+    {
+        public static void Seed(UserManager<IdentityUser> userManager,RoleManager<IdentityRole> roleManager)
+        {
+            SeedRoles(roleManager);
+            SeedUsers(userManager);
+        }
+
+        public static void SeedUsers(UserManager<IdentityUser> userManager)
+        {
+            if (userManager.FindByIdAsync("admin").Result == null)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = "admin",
+                    Email = "admiin@localhost.com"
+                };
+                var result = userManager.CreateAsync(user, "Admin1@test").Result;
+
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Administrator").Wait();
+
+                }
+            }
+        }
+
+        public static void SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            if (!roleManager.RoleExistsAsync("Administrator").Result)
+            {
+                var role = new IdentityRole { Name = "Administrator" };
+               var result = roleManager.CreateAsync(role).Result;
+            }
+            if (!roleManager.RoleExistsAsync("Employee").Result)
+            {
+                var role = new IdentityRole { Name = "Employee" };
+                var result = roleManager.CreateAsync(role).Result;
+            }
+        }
+
+
+    }
+}
